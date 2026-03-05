3717abe4388560420c6e4bfbdd188faad595b906
Jose Manuel Cavallera <120131770+manucavallera@users.noreply.github.com>
2026-03-04 22:00:44 -0300

    Simplify CORS configuration in main.ts

--- ms-nestjs-bussines/src/main.ts
+++ ms-nestjs-bussines/src/main.ts
@@ -3,36 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggingInterceptor } from './interceptors/loggins.interceptors';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalInterceptors(new LoggingInterceptor());
-
   const configService = app.get(ConfigService);
 
-  // 👉 DOMINIOS PERMITIDOS (CORS)
- const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3002',
-    'http://localhost:5173',
-    'https://ganaderia-pevs.vercel.app',
-    'https://manu-frontendganaderia.gygo4l.easypanel.host',
-    process.env.CORS_ORIGIN_DESARROLLO,
-  ].filter(Boolean);
-
   app.enableCors({
-    origin: (origin, callback) => {
-      // Permite llamadas sin origin (Postman, Thunder, Mobile Apps)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      console.error(`❌ CORS bloqueó origen en Business: ${origin}`);
-      return callback(new Error('Not allowed by CORS'), false);
-    },
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
@@ -43,14 +20,10 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/nestjs', app, document);
-
   const port = configService.get<number>('port') || 3000;
   await app.listen(port);
-
   console.log(`BUSSINES running on port ${port}`);
 }
-
 bootstrap();
